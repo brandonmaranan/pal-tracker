a8a1a2610d9d44885d08ffc73038792d2732c2ca
brandonmaranan <brandon.maranan@gmail.com>
2018-08-01 10:48:40 -0600

    Spring JDBC template lab - missing commits.

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -10,6 +10,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
+import javax.sql.DataSource;
+
 @SpringBootApplication
 public class PalTrackerApplication {
 
@@ -20,9 +22,12 @@ public class PalTrackerApplication {
 
 
     @Bean
-    TimeEntryRepository timeEntryRepository() {
-        return new InMemoryTimeEntryRepository();
+    TimeEntryRepository timeEntryRepository(DataSource dataSource) {
+        return new JdbcTimeEntryRepository(dataSource);
     }
+//    TimeEntryRepository timeEntryRepository() {
+//        return new InMemoryTimeEntryRepository();
+//    }
 
     @Bean
     public ObjectMapper jsonObjectMapper() {
